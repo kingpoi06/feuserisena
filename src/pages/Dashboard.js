7da00f8a47d76c132636d25503806cc3fb8297f1
kingpoi06 <putratyo90@gmail.com>
2024-04-29 12:25:51 +0700

    masi error refresh tokennya

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -20,15 +20,16 @@ const Dashboard = () => {
   const refreshToken = async () => {
     try {
       const response = await axios.get('https://apiuserisena.onrender.com/token');
-      setToken(response.data.accessToken);
-      const decoded = jwtDecode(response.data.accessToken);
+      const accessToken = response.data.accessToken;
+      setToken(accessToken);
+      const decoded = jwtDecode(accessToken);
       setUsername(decoded.username);
       setExpire(decoded.exp);
       setLoading(false);
     } catch (error) {
       console.error('Error refreshing token:', error);
       setLoading(false);
-      if(error.response){
+      if (error.response && error.response.status === 401) {
         navigate("/");
       }
     }
@@ -36,21 +37,16 @@ const Dashboard = () => {
 
   const axiosJWT = axios.create();
 
-  axiosJWT.interceptors.request.use(async(config) => {
+  axiosJWT.interceptors.request.use(async (config) => {
     const currentDate = new Date();
-    if(expire * 1000 < currentDate.getTime()){
-        const response = await axios.get('https://apiuserisena.onrender.com/token');
-        config.headers.Authorization = `Bearer ${response.data.accessToken}`;
-        setToken(response.data.accessToken);
-        const decoded = jwtDecode(response.data.accessToken);
-        setUsername(decoded.username);
-        setExpire(decoded.exp);
-        setLoading(false);
+    if (expire * 1000 < currentDate.getTime()) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   }, (error) => {
     return Promise.reject(error);
-  })
+  });
+
 
   return (
     <Layout>
